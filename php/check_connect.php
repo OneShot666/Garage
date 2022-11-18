872035dcec1319376c1222f7de3d2efb308a2f95
Nathan Mir <mir.nathan42@gmail.com>
2022-11-18 11:01:29 +1100

    test

---
+++ php/check_connect.php
@@ -0,0 +1,35 @@
+<?php
+    try {
+        $database = new PDO("mysql:host=localhost; dbname=garage", "root", "");
+        $database -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $database -> query("SELECT * FROM admin");
+    }
+    catch (Exception $e) {
+        die("Erreur : " . $e -> getMessage());
+    }
+
+    if (isset($_GET["envoyer"]) AND $_GET["envoyer"] == "Envoyer") {
+        $_GET["speudo"] = htmlspecialchars($_GET["speudo"]);  // Sécure contre failles
+        $speudo = $_GET["speudo"];
+        $speudo = strip_tags($speudo); // Supprime balises html
+        $_GET["password"] = htmlspecialchars($_GET["password"]);  // Sécure contre failles
+        $password = $_GET["password"];
+        $password = strip_tags($password); // Supprime balises html
+    }
+
+    $select_terme = $GLOBALS["select_terme"];
+    if (isset($terme)) {
+        $terme = strtolower($terme);  // Ecrit en minuscule
+        $select_terme = $database -> prepare("SELECT * FROM voiture WHERE marque LIKE ? or modèle LIKE ?");
+        $select_terme -> execute(array("%".$terme."%".$terme."%"));
+    } else {
+        $message = "Veuillez entrer votre requête dans la barre de recherche.";
+    }
+
+    while ($terme_trouve = $select_terme -> fetch()) {
+        echo "<div><h2>".$terme_trouve['marque']." : ".$terme_trouve['modèle']."</h2><p>"
+        .$terme_trouve."</p>";
+    }
+    $select_terme -> closeCursor();
+
+    // header("location:products.php");  // Renvoie vers page
---
+++ php/db_connexion.php
@@ -0,0 +1,6 @@
+<?php
+    $resultat = Database();
+    $database = $resultat[0];
+
+    $username = mysqli_real_escape_string($database, htmlspecialchars($_POST["username"]));
+    $password = mysqli_real_escape_string($database, htmlspecialchars($_POST["password"]));
